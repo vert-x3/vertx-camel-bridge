ff3a08ee422616c9ea1894a07df4ab8f02310483
Clement Escoffier <clement.escoffier@gmail.com>
2016-02-17 14:51:33 +0100

    cleanup repository

--- vertx-camel-bridge/src/main/java/io/vertx/ext/camel/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author <a href="http://escoffier.me">Clement Escoffier</a>
- */
-@ModuleGen(name="vertx-camel-bridge", groupPackage = "io.vertx.ext.camel")
-package io.vertx.ext.camel;
-
-import io.vertx.codegen.annotations.ModuleGen;
